f36a3c12ccd16cce3189e643541cd5ee67fa8546
Bryce Matsuda <brycematsuda@gmail.com>
2016-03-26 14:37:45 -0700

    Dominoes - only let player jump if touching ground or domino

--- Assets/Dominoes/Scripts/DomPlayerController.cs
+++ Assets/Dominoes/Scripts/DomPlayerController.cs
@@ -7,11 +7,12 @@ public class DomPlayerController : MonoBehaviour {
 	public Vector2 jumpHeight;
 	public bool canMove;
 	public bool touchGoal = false;
-
+	private bool letJump = true;
 	// Use this for initialization
 	void Start () {
 		canMove = true;
 		touchGoal = false;
+		letJump = true;
 	}
 	
 	// Update is called once per frame
@@ -19,14 +20,20 @@ public class DomPlayerController : MonoBehaviour {
 		if (canMove) {
 			Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, 0);
 			transform.position += move * speed * Time.deltaTime;
-		    if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))  //makes player jump
+		    if ((Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space)) &&
+		    	letJump)  //makes player jump
 		    {
 		    	GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);    
+		    	letJump = false;
 		    }
 		}
 	}
 
 	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Domino" || coll.gameObject.tag == "Start" || coll.gameObject.tag == "Finish") {
+			letJump = true;
+		}
+
         if (coll.gameObject.tag == "Goal") {
         	touchGoal = true;
         } else {
