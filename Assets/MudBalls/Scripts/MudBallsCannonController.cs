192a99b4b975be37490f9684cbd025bdffe5fcb5
Bryce Matsuda <brycematsuda@gmail.com>
2016-03-31 18:33:15 -0700

    Mud Balls - Add cannon with basic firing functionality

---
+++ Assets/MudBalls/Scripts/MudBallsCannonController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MudBallsCannonController : MonoBehaviour {
+
+	public Rigidbody2D bulletPrefab;
+	public GameObject fireSpot;
+
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.Space)) {
+			Fire();
+			Debug.Log ("Shoot");
+		}
+	}
+
+    // Fire a bullet
+	void Fire() {
+		Rigidbody2D bPrefab = Instantiate(bulletPrefab, fireSpot.transform.position, Quaternion.identity) as Rigidbody2D;
+		bPrefab.AddForce (Vector2.up * 1000f);
+	}
+}
