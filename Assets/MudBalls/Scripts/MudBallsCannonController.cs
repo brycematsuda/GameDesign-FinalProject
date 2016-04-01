d09130b85cb921a456cc7cde5edf85a9eaf4a52c
Bryce Matsuda <brycematsuda@gmail.com>
2016-03-31 22:07:56 -0700

    Mud balls - add single shot cannon fire.

--- Assets/MudBalls/Scripts/MudBallsCannonController.cs
+++ Assets/MudBalls/Scripts/MudBallsCannonController.cs
@@ -5,17 +5,21 @@ public class MudBallsCannonController : MonoBehaviour {
 
 	public Rigidbody2D bulletPrefab;
 	public GameObject fireSpot;
+	public float initSpeed;
+	public float angle;
 
 	void Update () {
-		if(Input.GetKeyDown (KeyCode.Space)) {
+		Debug.Log(GameObject.FindGameObjectsWithTag("Respawn").Length);
+		if(Input.GetKeyDown (KeyCode.Space) && GameObject.FindGameObjectsWithTag("Respawn").Length == 2) {
 			Fire();
-			Debug.Log ("Shoot");
 		}
 	}
 
     // Fire a bullet
 	void Fire() {
-		Rigidbody2D bPrefab = Instantiate(bulletPrefab, fireSpot.transform.position, Quaternion.identity) as Rigidbody2D;
-		bPrefab.AddForce (Vector2.up * 1000f);
+		Vector3 a = Quaternion.Euler(0, 0, angle) * Vector3.right;
+		Rigidbody2D shot = Instantiate(bulletPrefab, fireSpot.transform.position, Quaternion.identity) as Rigidbody2D;
+		shot.GetComponent<Rigidbody2D>().velocity = a * initSpeed;
+		shot.gravityScale = 3.0f;
 	}
 }
