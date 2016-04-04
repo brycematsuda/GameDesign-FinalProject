3dd51491d1a06d9758ebc3d51a5b5e73228a4336
travanti <nathantravanti@gmail.com>
2016-04-04 00:33:01 -0700

    Added Doors Prototype into assets folder
    This is the stable running prototype version of the Doors game,
    currently still uses basic shapes.

---
+++ Assets/_Doors/_Scripts/PlayerScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+    private Rigidbody rb;
+
+    [SerializeField]
+    private float speed;
+
+    [SerializeField]
+    private float runMult;
+
+    private bool speedChanged;
+
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        speedChanged = false;
+
+	}
+
+    //Called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //create quick way to make movement jump forward, use to simulate "dive" action
+            //float moveVertical = Input.GetAxis("Vertical");
+            speed = speed * runMult;
+            speedChanged = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            speed = speed / runMult;
+            speedChanged = false;
+        }
+
+            
+
+    }
+	
+	// FixedUpdate is called once per physics calculation
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.velocity = movement * speed;
+
+	
+	}
+}
