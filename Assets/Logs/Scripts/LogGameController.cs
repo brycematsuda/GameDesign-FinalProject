0ec1caf66a5b9931405112e3ea3d51327595e6d5
jordanywhite <whitejo16@up.edu>
2016-04-10 16:51:43 -0700

    Finally put log game on git

--- Assets/Logs/Scripts/LogGameController.cs
+++ Assets/Logs/Scripts/LogGameController.cs
@@ -1,15 +1,51 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LogGameController : MonoBehaviour {
 
+	public GameObject player;
+	public int lives;
+	private int score;
+	public Text liveGui;
+	public Text scoreGui;
+	public Text winLoseGui;
+	private bool isDisabled;
+
 	// Use this for initialization
 	void Start () {
-	
+		score = 99999;
+		liveGui.text = "Lives: " + lives.ToString();
+		scoreGui.text = "Score: " + score.ToString();
+		winLoseGui.text = "";
+		isDisabled = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		
+//		if (player.transform.position.y < -1.35) {
+//			if (!isDisabled) {
+//				player.GetComponent<PlayerController>().enabled = !player.GetComponent<UnityChan2DController>().enabled;
+//				isDisabled = true;
+//			}
+//
+//			winLoseGui.text = "Eliminated!";
+//			StartCoroutine(Wait(3));
+//		}
+//
+//		if (player.GetComponent<PlayerController>().touchGoal == true) {
+//			player.GetComponent<PlayerController>().canMove = false;
+//			winLoseGui.text = "Winner!";
+//			StartCoroutine(Wait(3));			
+//		}
+	}
+
+	IEnumerator Wait(float duration)
+	{
+		//This is a coroutine
+		yield return new WaitForSeconds(duration);   //Wait
+		lives -= 1;
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
 	}
 }
