95d4960de9fadd87c71d7706e9172d1b5480060f
Micah Alconcel <alconcel16@up.edu>
2016-04-11 03:56:18 -0700

    trailer stuff

---
+++ Assets/ScenicMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScenicMove : MonoBehaviour {
+
+	public GameObject target;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector3.MoveTowards (transform.position, target.transform.position, 2 * Time.deltaTime);
+	}
+}
--- Assets/UnityChan/Scripts/IdleChanger.cs
+++ Assets/UnityChan/Scripts/IdleChanger.cs
@@ -69,17 +69,24 @@ public class IdleChanger : MonoBehaviour
 				previousState = currentState;
 			}
 		}
+
+		if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+			anim.SetBool ("Back", true);
+		}
+		if (Input.GetKeyDown (KeyCode.RightArrow)) {
+			anim.SetBool ("Next", true);
+		}
 	}
 
 
-	void OnGUI()
-	{
-				GUI.Box(new Rect(Screen.width - 110 , 10 ,100 ,90), "Change Motion");
-				if(GUI.Button(new Rect(Screen.width - 100 , 40 ,80, 20), "Next"))
-					anim.SetBool ("Next", true);
-				if(GUI.Button(new Rect(Screen.width - 100 , 70 ,80, 20), "Back"))
-					anim.SetBool ("Back", true);
-	}
+//	void OnGUI()
+//	{
+//				GUI.Box(new Rect(Screen.width - 110 , 10 ,100 ,90), "Change Motion");
+//				if(GUI.Button(new Rect(Screen.width - 100 , 40 ,80, 20), "Next"))
+//					anim.SetBool ("Next", true);
+//				if(GUI.Button(new Rect(Screen.width - 100 , 70 ,80, 20), "Back"))
+//					anim.SetBool ("Back", true);
+//	}
 
 
 	// ランダム判定用関数
